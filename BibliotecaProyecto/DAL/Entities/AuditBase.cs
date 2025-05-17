a86f5dbea087668b70d8162e372d97b7b17491d8
juanjoctoro24 <juanjoctoro24@gmail.com>
2025-05-17 16:10:20 -0500

    LAYER DAL

---
+++ BibliotecaProyecto/DAL/Entities/AuditBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaProyecto.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key]//PK
+        [Required]//Obligatorio
+        public virtual Guid Id { get; set; } //PK de todas las tablas
+
+        public virtual DateTime? CreatedDate { get; set; } //para guardar todo registro nuevo con su date
+
+        public virtual DateTime? ModifiedDate { get; set; }//para guardar todo registro que se modifico con su date
+    }
+}
